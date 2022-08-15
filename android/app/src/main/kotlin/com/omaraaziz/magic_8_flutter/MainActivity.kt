9de1ae5024f30ff0abefc178200fa82115b6332c
Mennah Ayman <101893442+MennahAymann@users.noreply.github.com>
2022-08-15 05:10:21 +0200

    Magic 8 Ball

---
+++ android/app/src/main/kotlin/com/omaraaziz/magic_8_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.omaraaziz.magic_8_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      home: null,
+    ));
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
